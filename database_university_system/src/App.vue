b5405e908a9c48ae3189322469c0eb1dd94ca8a9
Kevin Bahari <Kevinrenaldibahari25@gmail.com>
2025-12-01 16:01:48 +0800

    vue main

--- database_university_system/src/App.vue
+++ database_university_system/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it! yeay" />
     </div>
   </header>
 
@@ -22,7 +22,10 @@ header {
   line-height: 1.5;
 }
 
-
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
 
 @media (min-width: 1024px) {
   header {
@@ -34,6 +37,7 @@ header {
   .logo {
     margin: 0 2rem 0 0;
   }
+
 
   header .wrapper {
     display: flex;
